c71f7f7fa78385ff0399f73df32889fafd8ce18c
HieuYeager <erenbeta3112@gmail.com>
2025-05-12 22:55:04 +0700

    thêm chức năng xem chi tiết thông tin nhân viên

--- Hotel Management/Controllers/StaffAccManagementController.cs
+++ Hotel Management/Controllers/StaffAccManagementController.cs
@@ -1,6 +1,7 @@
 using Hotel_Management.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -50,6 +51,38 @@ namespace Hotel_Management.Controllers
             //account.Status = !account.Status;
             db.SaveChanges();
             return RedirectToAction(nameof(Index));
+        }
+
+        // GET: StaffAccManagement/Details/{id}
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var staff = db.Staffs
+                .Include("s => s.Account")
+                .Where(s => s.AccountID == id)
+                .Select(s => new StaffViewModel
+                {
+                    AccountID = s.AccountID,
+                    Username = s.Account.Username,
+                    Fullname = s.FullName,
+                    CCCD = s.CCCD,
+                    Email = s.Email,
+                    Phone = s.Phone,
+                    Position = s.Position,
+                    Status = s.Account.Status
+                }).FirstOrDefault();
+
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(staff);
         }
     }
 }
